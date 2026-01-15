39d0ac56e17510318efe4a5735fd269734342242
Porcesco Stanislav <stanislav.porcesco@gmail.com>
2026-01-15 16:34:46 +0200

    Added a second email ofr the daily message

--- SMTP_Sending.py
+++ SMTP_Sending.py
@@ -1,13 +1,14 @@
 import random
 import smtplib
 from email.message import EmailMessage
-from time import sleep
 import os
 
 # --- CONFIGURARE DATE ---
 SENDER_EMAIL = os.environ.get("SENDER_EMAIL").strip()
 SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD").strip()
-RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL").strip()
+# --- RECEIVER EMAILS ---
+RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL1").strip()
+RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL2").strip()
 
 names = ["The King of Burgers Grigotita-Marius",
          "The Whopper Final Boss Grigorita-Marius",
@@ -16,20 +17,41 @@ names = ["The King of Burgers Grigotita-Marius",
          "The King's Morning Feast! Only at WhopperKing!"]
 chosen_name = random.choice(names)
 # --- CREAREA MESAJULUI ---
-msg = EmailMessage()
-msg['Subject'] = f"👑 {chosen_name} 👑"
-msg['From'] = SENDER_EMAIL
-msg['To'] = RECEIVER_EMAIL
-msg.set_content("Whopper, Whopper, Whopper! 🍔\n Start your day like a King, Marius, with a massive Triple Bacon Cheese Whopper paired with our famous large fries and an ice-cold drink! 🥓🍟🥤 \nClaim this royal feast now and we’ll even throw in a free side of golden onion rings if you order in the next 20 minutes! 👑✨ \nDon't wait—have it your way right away! 🏃‍♂️💨")
+msg_1 = EmailMessage()
+msg_1['Subject'] = f"👑 {chosen_name} 👑"
+msg_1['From'] = SENDER_EMAIL
+msg_1['To'] = RECEIVER_EMAIL
+msg_1.set_content("Whopper, Whopper, Whopper! 🍔\n "
+                  "Start your day like a King, Marius, "
+                  "with a massive Triple Bacon Cheese "
+                  "Whopper paired with our famous large "
+                  "fries and an ice-cold drink! 🥓🍟🥤 \n"
+                  "Claim this royal feast now and we’ll even "
+                  "throw in a free side of golden onion rings "
+                  "if you order in the next 20 minutes! 👑✨ \n"
+                  "Don't wait—have it your way right away! 🏃‍♂️💨")
+msg_2 = EmailMessage()
+msg_2['Subject'] = f"👑 {chosen_name} 👑"
+msg_2['From'] = SENDER_EMAIL
+msg_2['To'] = RECEIVER_EMAIL
+msg_2.set_content("Whopper, Whopper, Whopper! 🍔\n "
+                  "Start your day like a King, Marius, "
+                  "with a massive Triple Bacon Cheese "
+                  "Whopper paired with our famous large "
+                  "fries and an ice-cold drink! 🥓🍟🥤 \n"
+                  "Claim this royal feast now and we’ll even "
+                  "throw in a free side of golden onion rings "
+                  "if you order in the next 20 minutes! 👑✨ \n"
+                  "Don't wait—have it your way right away! 🏃‍♂️💨")
 # --- TRIMITEREA ---
 try:
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     print("Autentificare...")
     server.login(SENDER_EMAIL, SENDER_PASSWORD)
-    server.send_message(msg)
+    server.send_message(msg_1)
+    server.send_message(msg_2)
     server.quit()
     print("✅ SUCCES! Email-ul a fost trimis.")
-
 except Exception as e:
     print(f"❌ Eroarea este: {e}")
