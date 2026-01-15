8a28c37a3f74e451039a9a93101bc58db81a969f
Porcesco Stanislav <stanislav.porcesco@gmail.com>
2026-01-15 15:38:08 +0200

    Modified the message to be random

--- API_Sending.py
+++ API_Sending.py
@@ -3,7 +3,10 @@ import requests
 import random
 
 # Listă de nume pentru expeditor
-names = ["The King of Burgers Grigotita Marius", "The Whopper Final Boss Grigorita Marius", "Royal Grill Marius-Master", "Marius's Chef"]
+names = ["The King of Burgers Grigotita-Marius",
+         "The Whopper Final Boss Grigorita-Marius",
+         "Royal Grill Marius-Master", "Marius's Chef",
+         "Hexa Bacon Cheddar Whopper Lord Grigorita-Marius"]
 
 API_KEY = os.environ.get("BREVO_API_KEY")
 SENDER_EMAIL = os.environ.get("SENDER_EMAIL")
@@ -13,26 +16,14 @@ chosen_name = random.choice(names)
 url = "https://api.brevo.com/v3/smtp/email"
 
 payload = {
-    "sender": {"name": chosen_name, "email": SENDER_EMAIL},
-    "to": [{"email": RECEIVER_EMAIL}],
-    "subject": f"👑 Hi, {chosen_name}! 👑",
-    "htmlContent": "<h1>Whopper Time!</h1><p>Burgerul tau e gata.</p>"
-}
-
-payload = {
     "sender": {"name": "WhopperKing", "email": SENDER_EMAIL},
-    "to": [{"email": RECEIVER_EMAIL, "name": "Grigorita Marius The Yearly Whopper King!"}],
-    "subject": "👑 The King's Morning Feast! 👑",
-    "htmlContent": """
-        <html>
-            <body>
-                <h1>Whopper, Whopper, Whopper! 🍔</h1>
-                <p>Start your day like a King, Marius!</p>
-                <p>Penta Bacon Cheese Whopper with large fries is waiting. 🥓🍟</p>
-                <p>And grab some soda for the flavor!</p>
-            </body>
-        </html>
-    """
+    "to": [{"email": RECEIVER_EMAIL, "name": "Grigorita Marius"}],
+    "subject": f" {chosen_name} ",
+    "textContent": f"""Whopper, Whopper, Whopper! 
+Start your day like a King, Marius!
+Penta Bacon Cheese Whopper with large fries is waiting.
+And grab some soda for the flavor!
+"""
 }
 
 headers = {
@@ -41,5 +32,14 @@ headers = {
     "api-key": API_KEY
 }
 
-response = requests.post(url, json=payload, headers=headers)
-print(f"Status: {response.status_code}")
+try:
+    response = requests.post(url, json=payload, headers=headers)
+    if response.status_code == 201:
+        print(f"✅ SUCCES! Email-ul a fost trimis cu succes.")
+        print(f"ID Mesaj: {response.json().get('messageId')}")
+    else:
+        print(f"❌ EROARE API: Cod status {response.status_code}")
+        print(f"Detalii eroare: {response.text}")
+
+except Exception as e:
+    print(f"❌ EROARE CRITICĂ: Nu s-a putut contacta serverul Brevo. Detalii: {e}")
--- SMTP_Sending.py
+++ SMTP_Sending.py
@@ -1,3 +1,4 @@
+import random
 import smtplib
 from email.message import EmailMessage
 from time import sleep
@@ -8,9 +9,15 @@ SENDER_EMAIL = os.environ.get("SENDER_EMAIL").strip()
 SENDER_PASSWORD = os.environ.get("SENDER_PASSWORD").strip()
 RECEIVER_EMAIL = os.environ.get("RECEIVER_EMAIL").strip()
 
+names = ["The King of Burgers Grigotita-Marius",
+         "The Whopper Final Boss Grigorita-Marius",
+         "Royal Grill Marius-Master", "Marius's Chef",
+         "Hexa Bacon Cheddar Whopper Lord Grigorita-Marius",
+         "The King's Morning Feast! Only at WhopperKing!"]
+chosen_name = random.choice(names)
 # --- CREAREA MESAJULUI ---
 msg = EmailMessage()
-msg['Subject'] = "👑 The King's Morning Feast! Only at WhopperKing! 👑"
+msg['Subject'] = f"👑 {chosen_name} 👑"
 msg['From'] = SENDER_EMAIL
 msg['To'] = RECEIVER_EMAIL
 msg.set_content("Whopper, Whopper, Whopper! 🍔\n Start your day like a King, Marius, with a massive Triple Bacon Cheese Whopper paired with our famous large fries and an ice-cold drink! 🥓🍟🥤 \nClaim this royal feast now and we’ll even throw in a free side of golden onion rings if you order in the next 20 minutes! 👑✨ \nDon't wait—have it your way right away! 🏃‍♂️💨")
@@ -22,7 +29,6 @@ try:
     server.login(SENDER_EMAIL, SENDER_PASSWORD)
     server.send_message(msg)
     server.quit()
-
     print("✅ SUCCES! Email-ul a fost trimis.")
 
 except Exception as e:
